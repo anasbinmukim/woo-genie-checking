463975580abb467613f42fbe98fea6223337cbe6
Anas <anasbinmukim@gmail.com>
2017-11-21 18:20:19 +0600

    Thank you hook update

--- inc/class-wc-gateway-genie-checking.php
+++ inc/class-wc-gateway-genie-checking.php
@@ -42,7 +42,7 @@ class WC_Gateway_Genie_Checking extends WC_Payment_Gateway {
 		// Actions
 		add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
 
-		add_action( 'woocommerce_thankyou_invoiceme', array( $this, 'thankyou_page' ) );
+		add_action( 'woocommerce_thankyou_' . $this->id, array( $this, 'thankyou_page' ) );
 
 		// Customer Emails
 		add_action( 'woocommerce_email_before_order_table', array( $this, 'email_instructions' ), 10, 3 );
